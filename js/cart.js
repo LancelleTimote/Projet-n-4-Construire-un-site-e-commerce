2cf45ac4717bbccf33b699bb05f7172ece1c5121
Timoté LANCELLE <timote.lancelle@outlook.fr>
2021-04-10 11:24:44 +0200

    Modification alerte formulaire en dessous du champ, ajout couleur verte et rouge, test fonction post

--- js/cart.js
+++ js/cart.js
@@ -4,7 +4,7 @@ let productSaveInLocalStorage = JSON.parse(localStorage.getItem('cart'));
 console.log(productSaveInLocalStorage);
 
 //----------------------------------------L'affichage des produits du panier----------------------------------------
-//Séléction de la class où je vais injecter le code HTML
+//Séléction de l'élément où je vais injecter le code HTML
 const cart = document.getElementById('cart');
 console.log(cart);
 
@@ -193,48 +193,58 @@ const displayFormHtml = () => {
             <div class="form-group col-md-6">
                 <label for="lastname">Nom :</label>
                 <input name="lastname" type="text" id="lastname" class="form-control" minlength="2" maxlength="30" placeholder="exemple : Dupont">
+                <div class="invalid-feedback text-danger" id="textIncorrectLastName"></div>
             </div>
             <div class="form-group col-md-6">
-                <label for="name">Prénom :</label>
-                <input name="name" type="text" id="name" class="form-control" minlength="2" maxlength="30" placeholder="exemple : John">
+                <label for="firstname">Prénom :</label>
+                <input name="firstname" type="text" id="firstname" class="form-control" minlength="2" maxlength="30" placeholder="exemple : John">
+                <div class="invalid-feedback text-danger" id="textIncorrectFirstName"></div>
             </div>
         </div>
         <div class="form-row">
             <div class="form-group col">
                 <label for="address">Adresse :</label>
                 <input name="address" type="text" id="address" class="form-control" placeholder="exemple : 14 rue des fleurs">
+                <div class="invalid-feedback text-danger" id="textIncorrectAddress"></div>
             </div>
         </div>
         <div class="form-row">
             <div class="form-group col">
                 <label for="address2">Compléments d'adresse :</label>
                 <input name="address2" type="text" id="address2" class="form-control" placeholder="exemple : numéro bâtiment, appartement, etc...">
+                <div class="invalid-feedback text-danger" id="textIncorrectAddress2"></div>
             </div>
         </div>
         <div class="form-row">
             <div class="form-group col-md-6">
                 <label for="city">Ville :</label>
                 <input name="city" type="text" id="city" class="form-control" placeholder="exemple : Paris">
+                <div class="invalid-feedback text-danger" id="textIncorrectCity"></div>
             </div>
             <div class="form-group col-md-6">
                 <label for="zipCode">Code postal :</label>
                 <input name="zipCode" type="number" id="zipCode" class="form-control" placeholder="exemple : 75680">
+                <div class="invalid-feedback text-danger" id="textIncorrectZipCode"></div>
             </div>
         </div>
         <div class="form-row">
             <div class="form-group col">
                 <label for="country">Pays :</label>
                 <input name="country" type="text" id="country" class="form-control" placeholder="exemple : France">
+                <div class="invalid-feedback text-danger" id="textIncorrectCountry"></div>
             </div>
         </div>
         <div class="form-row">
+
             <div class="form-group col-md-6">
                 <label for="email">Mail :</label>
                 <input name="email" type="text" id="email" class="form-control" placeholder="exemple : oriteddy@orinoco.com">
+                <div class="invalid-feedback text-danger" id="textIncorrectEmail"></div>
             </div>
             <div class="form-group col-md-6">
                 <label for="phone">Téléphone :</label>
                 <input name="phone" type="tel" id="phone" class="form-control" placeholder="exemple : 0753851239">
+                <div class="invalid-feedback text-danger" id="textIncorrectPhone"></div>
             </div>
         </div>
         <div class="text-center">
@@ -263,7 +273,7 @@ btnSendForm.addEventListener("click", (e) => {
     class Form {
         constructor() {
             this.lastname = document.querySelector("#lastname").value;
-            this.name = document.querySelector("#name").value;
+            this.firstname = document.querySelector("#firstname").value;
             this.address = document.querySelector("#address").value;
             this.address2 = document.querySelector("#address2").value;
             this.city = document.querySelector("#city").value;
@@ -283,116 +293,175 @@ btnSendForm.addEventListener("click", (e) => {
 
     //----------Gestion des regex et des alertes----------
     const textAlert = (value) => {
-        return `${value} : veuillez entrer une valeur valide.`;
+        return `${value} : veuillez remplir correctement ce champ.`;
     }
 
-    const regExLastnameNameCityCountry = (value) => {
+    const regExLastnameFirstnameCityCountry = (value) => {
         return /^\S[a-z ,.'à-ÿ-]+$/.test(value);    //la méthode test() vérifie s'il y a une correspondance entre un texte et une expression rationnelle.
     }                                               //elle retourne true en succès, et false en cas contraire (booléen).
 
+    //**********Fin gestion des regex et des alertes**********
+
+    //----------fonction pour gérer l'affichage du texte en dessous de l'input pour indiquer qu'il faut le remplir correctement----------
+    function textCorrectFormField(querySelectorId) {
+        document.querySelector(`#${querySelectorId}`).textContent = "";
+    }
+
+    function textIncorrectFormField(querySelectorId) {
+        document.querySelector(`#${querySelectorId}`).textContent = "Veuillez remplir correctement ce champ.";
+    }
+
+    //**********Fin fonction pour gérer l'affichage du texte en dessous de l'input pour indiquer qu'il faut le remplir correctement**********
+
+    //----------fonction pour gérer l'affichage de la couleur de l'input si correct ou incorrect----------
+    function colorCorrectFormField(querySelectorId) {
+        document.querySelector(`#${querySelectorId}`).classList.add("is-valid");
+        document.querySelector(`#${querySelectorId}`).classList.remove("is-invalid");
+    }
+
+    function colorIncorrectFormField(querySelectorId) {
+        document.querySelector(`#${querySelectorId}`).classList.add("is-invalid");
+    }
+
+    //**********Fin fonction pour gérer l'affichage de la couleur de l'input si correct ou incorrect**********
+
+    //Contrôle de la validité du nom
     function lastNameControl() {
-        //Contrôle de la validité du nom
         const theLastName = formValues.lastname;
-        if(regExLastnameNameCityCountry(theLastName)) {
+        if(regExLastnameFirstnameCityCountry(theLastName)) {
+            colorCorrectFormField("lastname");
+            textCorrectFormField("textIncorrectLastName");
             return true;
         }else{
+            colorIncorrectFormField("lastname");
+            textIncorrectFormField("textIncorrectLastName");
             alert(textAlert("Nom incorrect"));
             return false;
         }
     }
 
-    function nameControl() {
-        //Contrôle de la validité du prénom
-        const theName = formValues.name;
-        if(regExLastnameNameCityCountry(theName)) {
+    //Contrôle de la validité du prénom
+    function firstNameControl() {
+        const theFirstName = formValues.firstname;
+        if(regExLastnameFirstnameCityCountry(theFirstName)) {
+            colorCorrectFormField("firstname");
+            textCorrectFormField("textIncorrectFirstName");
             return true;
         }else{
+            colorIncorrectFormField("firstname");
+            textIncorrectFormField("textIncorrectFirstName");
             alert(textAlert("Prénom incorrect"));
             return false;
         }
     }
 
+    //Contrôle de la validité de l'adresse
     function addressControl() {
-        //Contrôle de la validité de l'adresse
         const theAddress = formValues.address;
         if(/^[0-9]{1,5}[A-z0-9 'à-ÿ-]{5,40}$/.test(theAddress)) {
+            colorCorrectFormField("address");
+            textCorrectFormField("textIncorrectAddress");
             return true;
         }else{
-            alert("Addresse incorrect : veuillez entrer une adresse valide.");
+            colorIncorrectFormField("address");
+            textIncorrectFormField("textIncorrectAddress");
+            alert(textAlert("Addresse incorrect"));
             return false;
         }
     }
 
+    //Contrôle de la validité des compléments d'adresse
     function address2Control() {
-        //Contrôle de la validité des compléments d'adresse
         const theAddress2 = formValues.address2;
         if(/^[A-z0-9 'à-ÿ-]{5,30}$/.test(theAddress2)) {
+            colorCorrectFormField("address2");
+            textCorrectFormField("textIncorrectAddress2");
             return true;
         }else{
-            alert("Compléments d'adresse incorrect : veuillez entrer des valeurs valides.");
+            colorIncorrectFormField("address2");
+            textIncorrectFormField("textIncorrectAddress2");
+            alert(textAlert("Compléments d'adresse incorrect"));
             return false;
         }
     }
 
+    //Contrôle de la validité de la ville
     function cityControl() {
-        //Contrôle de la validité de la ville
         const theCity = formValues.city;
-        if(regExLastnameNameCityCountry(theCity)) {
+        if(regExLastnameFirstnameCityCountry(theCity)) {
+            colorCorrectFormField("city");
+            textCorrectFormField("textIncorrectCity");
             return true;
         }else{
+            colorIncorrectFormField("city");
+            textIncorrectFormField("textIncorrectCity");
             alert(textAlert("Ville incorrect"));
             return false;
         }
     }
 
+    //Contrôle de la validité du code postal
     function zipCodeControl() {
-        //Contrôle de la validité du code postal
         const theZipCode = formValues.zipCode;
         if(/^[0-9]{5}$/.test(theZipCode)) {
+            colorCorrectFormField("zipCode");
+            textCorrectFormField("textIncorrectZipCode");
             return true;
         }else{
-            alert("Code postal incorrect : doit être composé de 5 chiffres.");
+            colorIncorrectFormField("zipCode");
+            textIncorrectFormField("textIncorrectZipCode");
+            alert(textAlert("Code postal incorrect"));
             return false;
         }
     }
 
+    //Contrôle de la validité du pays
     function countryControl() {
-        //Contrôle de la validité du pays
         const theCountry = formValues.country;
-        if(regExLastnameNameCityCountry(theCountry)) {
+        if(regExLastnameFirstnameCityCountry(theCountry)) {
+            colorCorrectFormField("country");
+            textCorrectFormField("textIncorrectCountry");
             return true;
         }else{
+            colorIncorrectFormField("country");
+            textIncorrectFormField("textIncorrectCountry");
             alert(textAlert("Pays incorrect"));
             return false;
         }
     }
 
+    //Contrôle de la validité de l'email
     function emailControl() {
-        //Contrôle de la validité de l'email
         const theEmail = formValues.email;
         if(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(theEmail)) {
+            colorCorrectFormField("email");
+            textCorrectFormField("textIncorrectEmail");
             return true;
         }else{
-            alert("Email incorrect : veuillez entrer un email valide.");
+            colorIncorrectFormField("email");
+            textIncorrectFormField("textIncorrectEmail");
+            alert(textAlert("Email incorrect"));
             return false;
         }
     }
 
+    //Contrôle de la validité du téléphone
     function phoneControl() {
-        //Contrôle de la validité du téléphone
         const thePhone = formValues.phone;
         if(/^[(]{0,1}[0-9]{3}[)]{0,1}[-\s\.]{0,1}[0-9]{3}[-\s\.]{0,1}[0-9]{4}$/.test(thePhone)) {
+            colorCorrectFormField("phone");
+            textCorrectFormField("textIncorrectPhone");
             return true;
         }else{
-            alert("Téléphone incorrect : veuillez entrer un numéro de téléphone valide.");
+            colorIncorrectFormField("phone");
+            textIncorrectFormField("textIncorrectPhone");
+            alert(textAlert("Téléphone incorrect"));
             return false;
         }
     }
 
-    //**********Fin de la gestion des regex et des alertes**********
-
     //Contrôle validité formulaire avant envoie dans le localStorage
-    if(lastNameControl() && nameControl() && addressControl() && address2Control() && cityControl() && zipCodeControl() && countryControl() && emailControl() && phoneControl()) {
+    if(lastNameControl() && firstNameControl() && addressControl() && address2Control() && cityControl() && zipCodeControl() && countryControl() && emailControl() && phoneControl()) {
         //Mettre l'objet "formValues" dans le localStorage
         localStorage.setItem("formValues", JSON.stringify(formValues));
     }else{
@@ -402,26 +471,33 @@ btnSendForm.addEventListener("click", (e) => {
     //********************Fin gestion validation formulaire********************
     
     //Mettre les values du formulaire, les produits sélectionnés, et le total du panier dans un objet à envoyer au serveur
-    const toSendServer = {
-        productSaveInLocalStorage,
-        formValues,
-        totalPrice,
-    }
-    console.log("toSendServer");
-    console.log(toSendServer);
     
-    //Requête POST, envoie de l'objet "toSendServer" au serveur
-    // function postOrder(toSendServer) {
-    //     fetch("http://localhost:3000/api/teddies/order", {
-    //         method: 'POST',
-    //         headers: {
-    //             'Content-Type': 'application/json'
-    //         },
-    //         mode:'cors',
-    //         body: JSON.stringify(toSendServer),
-    //     })
+    
+    // const toSendServer = {
+    //     productSaveInLocalStorage,
+    //     formValues,
+    //     totalPrice,
     // }
-    // postOrder(toSendServer);
+    
+    // Requête POST, envoie de l'objet "toSendServer" au serveur
+    let contact = {
+        firstName: "dupont",
+        lastName: "phil",
+        address: "14 rue des fleurs",
+        city: "paris",
+        email: "orinoco@oriteddy.fr",
+    };
+    let products = ['5be9c8541c9d440000665243'];
+    let data = { contact, products };
+
+    fetch('http://localhost:3000/api/teddies/order', {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {"Content-type": "application/json; charset=UTF-8"}
+    })
+    .then(response => response.json()) 
+    .then(json => console.log(json))
+    .catch(err => console.log(err));
 })
 
 //********************Fin addEventListener********************
@@ -436,11 +512,15 @@ const dataLocalStorageObjet = JSON.parse(dataLocalStorage);
 
 //Fonction pour que le champ du formulaire soit rempli par les données du localStorage si elles existent
 function fillInputWithLocalStorage(input) {
-    document.querySelector(`#${input}`).value = dataLocalStorageObjet[input];
+    if(dataLocalStorageObjet == null) {
+        
+    }else{
+        document.querySelector(`#${input}`).value = dataLocalStorageObjet[input];
+    }
 }
 
 fillInputWithLocalStorage("lastname");
-fillInputWithLocalStorage("name");
+fillInputWithLocalStorage("firstname");
 fillInputWithLocalStorage("address");
 fillInputWithLocalStorage("address2");
 fillInputWithLocalStorage("city");
@@ -448,9 +528,6 @@ fillInputWithLocalStorage("zipCode");
 fillInputWithLocalStorage("country");
 fillInputWithLocalStorage("email");
 fillInputWithLocalStorage("phone");
-
-console.log("dataLocalStorageObjet");
-console.log(dataLocalStorageObjet);
 
 //****************************************Fin du formulaire de commande****************************************
 
